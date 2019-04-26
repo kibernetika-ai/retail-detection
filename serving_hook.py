e1409a7ffd77835a9ce1bba49a71201785b4b7b9
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-04-26 15:10:21 +0400

    Working example

--- serving_hook.py
+++ serving_hook.py
@@ -1,82 +1,128 @@
 import base64
-import io
 import json
 import logging
 
+import cv2
 import numpy as np
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 
 
 LOG = logging.getLogger(__name__)
-threshold = 0.7
+PARAMS = {
+    'device': 'CPU',
+    'threshold': 0.7,
+    'output_type': 'bytes',
+    'need_table': True,
+    'target_size': (544, 320)
+}
 
 
 def init_hook(**params):
-    fac = params.get('threshold')
-    if fac:
-        fac = float(fac)
-        global threshold
-        threshold = fac
-        LOG.info('Threshold is "%s" now.' % threshold)
+    global PARAMS
+    PARAMS.update(params)
+
+    PARAMS['threshold'] = float(PARAMS['threshold'])
+
+    PARAMS['need_table'] = _boolean_string(PARAMS['need_table'])
+    LOG.info('Init with params:')
+    LOG.info(json.dumps(PARAMS, indent=2))
 
 
-def preprocess(inputs, ctx, **kwargs):
-    image = inputs.get('input')
+def _boolean_string(s):
+    if isinstance(s, bool):
+        return s
+
+    s = s.lower()
+    if s not in {'false', 'true'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'true'
+
+
+def _load_image(inputs, image_key):
+    image = inputs.get(image_key)
     if image is None:
-        raise RuntimeError('Missing "input" key in inputs. Provide an image in "input" key')
+        raise RuntimeError('Missing "{0}" key in inputs. Provide an image in "{0}" key'.format(image_key))
 
-    if isinstance(image[0], (six.string_types, bytes)):
-        image = Image.open(io.BytesIO(image[0]))
+    if len(image.shape) == 0:
+        image = np.stack([image.tolist()])
 
-        image = image.convert('RGB')
+    if len(image.shape) < 3:
+        image = cv2.imdecode(np.frombuffer(image[0], np.uint8), cv2.IMREAD_COLOR)
 
-    if isinstance(image, np.ndarray) and image.shape[2] == 4:
-        # Convert RGBA -> RGB
-        rgba_image = Image.fromarray(image)
-        image = rgba_image.convert('RGB')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    data = image.resize((300, 300), Image.ANTIALIAS)
-    data = np.array(data).transpose([2, 0, 1]).reshape(1, 3, 300, 300)
+    return image
+
+
+def process(inputs, ctx, **kwargs):
+    frame = _load_image(inputs, 'input')
     # convert to BGR
-    data = data[:, ::-1, :, :]
+    data = frame[:, :, ::-1]
+    data = cv2.resize(data, PARAMS['target_size'], interpolation=cv2.INTER_AREA)
 
-    ctx.image = image
+    # convert to input shape (N, C, H, W)
+    data = np.expand_dims(np.transpose(data, [2, 0, 1]), axis=0)
 
-    return {'data': data}
+    input_name = list(kwargs['model_inputs'])[0]
+    outputs = ctx.driver.predict({input_name: data})
 
+    outputs = list(outputs.values())[0].reshape([-1, 7])
+    # 7 values:
+    # class_id, label, confidence, x_min, y_min, x_max, y_max
+    # Select boxes where confidence > factor
+    outputs = outputs.reshape(-1, 7)
+    bboxes_raw = outputs[outputs[:, 2] > PARAMS['threshold']]
+    bounding_boxes = bboxes_raw[:, 3:7]
+    bounding_boxes[:, 0] = bounding_boxes[:, 0] * frame.shape[1]
+    bounding_boxes[:, 2] = bounding_boxes[:, 2] * frame.shape[1]
+    bounding_boxes[:, 1] = bounding_boxes[:, 1] * frame.shape[0]
+    bounding_boxes[:, 3] = bounding_boxes[:, 3] * frame.shape[0]
+    bounding_boxes = bounding_boxes.astype(int)
 
-def draw_rectangle(draw, coordinates, color, width=1):
-    for i in range(width):
-        rect_start = (coordinates[0][0] - i, coordinates[0][1] - i)
-        rect_end = (coordinates[1][0] + i, coordinates[1][1] + i)
-        draw.rectangle((rect_start, rect_end), outline=color)
+    result = {'person_boxes': bounding_boxes, 'person_scores': bboxes_raw[:, 2]}
+    if len(bounding_boxes) > 0:
+        table = result_table_string(result, frame)
+        add_overlays(frame, bounding_boxes, labels=None)
+    else:
+        table = []
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    if PARAMS['output_type'] == 'bytes':
+        image_bytes = cv2.imencode(".jpg", frame, params=[cv2.IMWRITE_JPEG_QUALITY, 95])[1].tostring()
+    else:
+        image_bytes = frame
+
+    return {
+        'output': image_bytes,
+        'table_output': table
+    }
 
 
 def add_overlays(frame, boxes, labels=None):
-    draw = ImageDraw.Draw(frame)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
     if boxes is not None:
         for i, face in enumerate(boxes):
             face_bb = face.astype(int)
-            draw_rectangle(
-                draw,
-                [(face_bb[0], face_bb[1]), (face_bb[2], face_bb[3])],
-                (250, 0, 250), width=4
+            cv2.rectangle(
+                frame,
+                (face_bb[0], face_bb[1]), (face_bb[2], face_bb[3]),
+                (0, 250, 0), thickness=2
             )
 
             if labels:
-                draw.text(
+                cv2.putText(
+                    frame,
+                    labels[i],
                     (face_bb[0] + 4, face_bb[1] + 5),
-                    labels[i], font=ImageFont.load_default(),
+                    font, 1.0, (0, 250, 0), thickness=1, lineType=cv2.LINE_AA,
                 )
 
 
-def image_resize(image, width=None, height=None, inter=Image.ANTIALIAS):
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
     # grab the image size
-    dim = None
-    (h, w) = image.height, image.width
+    (h, w) = image.shape[0], image.shape[1]
 
     # if both the width and height are None, then return the
     # original image
@@ -98,28 +144,31 @@ def image_resize(image, width=None, height=None, inter=Image.ANTIALIAS):
         dim = (width, int(h * r))
 
     # resize the image
-    resized = image.resize(dim, inter)
+    resized = cv2.resize(image, dim, interpolation=inter)
 
     # return the resized image
     return resized
 
 
-def result_table_string(result_dict, ctx):
+def result_table_string(result_dict, frame):
     table = []
+    w, h = frame.shape[1], frame.shape[0]
 
-    def crop_from_box(box, normalized_coordinates=True):
-        left, right = box[0], box[2]
-        top, bottom = box[1], box[3]
+    def crop_from_box(box, normalized_coordinates=False):
+        left, right = max(0, box[0]), min(box[2], w)
+        top, bottom = max(0, box[1]), min(box[3], h)
         if normalized_coordinates:
-            left, right = left * ctx.image.width, right * ctx.image.width
-            top, bottom = top * ctx.image.height, bottom * ctx.image.height
+            left, right = left * w, right * w
+            top, bottom = top * h, bottom * h
 
-        cropped = ctx.image.crop((left, top, right, bottom))
-        cropped = image_resize(cropped, width=256)
-        image_bytes = io.BytesIO()
-        cropped.convert('RGB').save(image_bytes, format='JPEG', quality=80)
+        cropped = frame[top:bottom, left:right]
+        # max size width 128
+        if cropped.shape[1] > 128:
+            cropped = image_resize(cropped, height=128)
+        cim = cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR)
+        image_bytes = cv2.imencode(".jpg", cim, params=[cv2.IMWRITE_JPEG_QUALITY, 95])[1].tostring()
 
-        return image_bytes.getvalue()
+        return image_bytes
 
     def append(type_, name, prob, image):
         encoded = image
@@ -135,46 +184,8 @@ def result_table_string(result_dict, ctx):
             }
         )
 
-    if len(result_dict.get('face_boxes', [])) > 0:
-        for prob, box in zip(result_dict['face_scores'], result_dict['face_boxes']):
-            append('face', 'face', prob, crop_from_box(box))
+    if len(result_dict.get('person_boxes', [])) > 0:
+        for prob, box in zip(result_dict['person_scores'], result_dict['person_boxes']):
+            append('person', 'person', prob, crop_from_box(box))
 
     return json.dumps(table)
-
-
-def postprocess(outputs, ctx):
-    outputs = list(outputs.values())[0].reshape([-1, 7])
-    # 7 values:
-    # class_id, label, confidence, x_min, y_min, x_max, y_max
-    # Select boxes where confidence > factor
-    bboxes_raw = outputs[outputs[:, 2] > threshold]
-
-    w, h = ctx.image.size
-
-    if bboxes_raw is not None:
-        bboxes = np.zeros([len(bboxes_raw), 4])
-        labels = []
-        for i, box in enumerate(bboxes_raw):
-            bboxes[i][0] = box[3] * w
-            bboxes[i][1] = box[4] * h
-            bboxes[i][2] = box[5] * w
-            bboxes[i][3] = box[6] * h
-            labels.append('%.3f. %s' % (box[2], box[1]))
-
-        result = {'face_boxes': bboxes_raw[:, 3:7], 'face_scores': bboxes_raw[:, 2]}
-        table = result_table_string(result, ctx)
-        add_overlays(ctx.image, bboxes, labels=None)
-    else:
-        table = []
-
-    image_bytes = io.BytesIO()
-
-    im = ctx.image
-    im.save(image_bytes, format='JPEG', quality=80)
-
-    return {
-        'output': image_bytes.getvalue(),
-        'table_output': table
-        # 'boxes': ctx.bounding_boxes,
-        # 'labels': np.array(labels)
-    }
